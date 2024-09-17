0711535f74dd236c5d55bad74cccf004a8daae5a
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2024-09-17 20:00:10 +0200

    revert

--- model.py
+++ model.py
@@ -1,19 +1,28 @@
-import torch
 import torch.nn as nn
+import pandas as pd
 
-class LSTM(nn.Module):
-    def __init__(self, hidden_size: int):
-        super(LSTM, self).__init__()
-        self.output_size = 1
-        self.num_layers = 1
-        self.input_size = 3
-        self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(self.input_size, self.hidden_size)
-        self.fc = nn.Linear(self.hidden_size, self.output_size)
+data = pd.read_csv("HVAC-hour_cleaned.csv").values
+
+class TestModel(nn.Module):
+    def __init__(self, stacked_layers: int, hidden_size: int):
+        super().__init__()
+        self.lstm = nn.LSTM(stacked_layers, hidden_size, stacked_layers)
+        self.label = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, self.hidden_size)
-        c0 = torch.zeros(self.num_layers, self.hidden_size)
-        out, _ = self.lstm(x, (h0, c0))
-        out = out[-1, :]
-        return self.fc(out).squeeze()
+        out, (hf, cf) = self.lstm(data)
+        return self.label(out[-1])
+
+model = TestModel(1, 2)
+
+print(model)
+
+def trainer(model, epoch):
+    model.train()
+
+    for i in range(epoch): 
+        for j in range(len(data)):
+            pass
+
+
+trainer(model, 10)
