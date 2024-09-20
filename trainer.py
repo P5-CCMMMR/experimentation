a1bbe0a1683ea0da9160c12bd7e494f010b7c59d
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2024-09-20 22:50:29 +0200

    small refactors

--- trainer.py
+++ trainer.py
@@ -12,12 +12,12 @@ from torch.utils.tensorboard import SummaryWriter
 matplotlib.use("Agg")
 
 # Hyper params
-hidden_size = 32
+hidden_size = 64
 epochs = 1
 learning_rate = 0.005
 seq_len = 4
 batch_size = 64
-num_workers = 8
+num_workers = 13
 
 def create_sequences(features: np.ndarray, seq_len: int):
     num_sequences = len(features) - seq_len
@@ -36,7 +36,7 @@ class SimpleLSTM(nn.Module):
         super(SimpleLSTM, self).__init__()
         self.input_size = 3
         self.output_size = 1
-        self.num_layers = 1
+        self.num_layers = 32
         self.batch_size = batch_size
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True)
@@ -59,9 +59,6 @@ class LSTMTrainer(L.LightningModule):
         self.writer = SummaryWriter()
         self.all_predictions = []
         self.all_actuals = []
-    
-    def forward(self, x):
-        return self.model(x)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
@@ -69,22 +66,22 @@ class LSTMTrainer(L.LightningModule):
 
     def training_step(self, batch):
         x, y = batch
-        prediction = self(x)
-        loss = torch.sqrt(self.criterion(prediction, y))
+        prediction = self.model(x)
+        loss = self.criterion(prediction, y)
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def test_step(self, batch):
         x, y = batch
-        prediction = self(x)
-        loss = self.criterion(prediction, y)
+        predictions = self.model(x)
+        print(predictions)
+        print(y)
+        loss = self.criterion(predictions, y)
         self.log('test_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
         # Accumulate predictions and actuals
-        self.all_predictions.extend(prediction.detach().cpu().numpy().flatten())
+        self.all_predictions.extend(predictions.detach().cpu().numpy().flatten())
         self.all_actuals.extend(y.detach().cpu().numpy().flatten())
-
-        return loss
 
     def on_test_epoch_end(self):
         min_length = min(len(self.timestamps), len(self.all_predictions), len(self.all_actuals))
@@ -92,18 +89,16 @@ class LSTMTrainer(L.LightningModule):
         predictions = self.all_predictions[:min_length]
         actuals = self.all_actuals[:min_length]
 
-        plt.figure(figsize=(40, 20), dpi=300)
         plt.plot(timestamps, predictions, label="Predictions")
         plt.plot(timestamps, actuals, label="Actual")
         plt.xlabel("Time")
         plt.ylabel("Indoor Temperature")
-        plt.title("Predictions vs Actual")
+        plt.title("Predictions vs actuals")
         plt.legend()
         plt.grid()
         plt.gcf().autofmt_xdate()
 
         self.writer.add_figure("predictions", plt.gcf(), global_step=self.current_epoch)
-        plt.close()
 
 model = SimpleLSTM(hidden_size, batch_size)
 
@@ -114,13 +109,12 @@ test_data = dfv[train_size:]
 
 training_features = train_data[:, 1:]
 training_xs, training_ys = create_sequences(training_features, seq_len)
-training_loader = data.DataLoader(data.TensorDataset(training_xs, training_ys), batch_size=batch_size, drop_last=True, num_workers=8)
+training_loader = data.DataLoader(data.TensorDataset(training_xs, training_ys), batch_size=batch_size, drop_last=True, num_workers=num_workers)
 
 test_features = test_data[:, 1:]
 test_xs, test_ys = create_sequences(test_features, seq_len)
-test_loader = data.DataLoader(data.TensorDataset(test_xs, test_ys), batch_size=batch_size, drop_last=True, num_workers=8)
+test_loader = data.DataLoader(data.TensorDataset(test_xs, test_ys), batch_size=batch_size, drop_last=True, num_workers=num_workers)
 timestamps = test_data[:, 0]
-
 trainer = LSTMTrainer(model, learning_rate, timestamps)
 lightning_trainer = L.Trainer(max_epochs=epochs)
 lightning_trainer.fit(trainer, training_loader)
