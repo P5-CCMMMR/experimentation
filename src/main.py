0204028573d970765709ed4a7253c1c79b496938
MadsVind <madsvind7@gmail.com>
2024-09-23 16:49:17 +0200

    main change

--- src/main.py
+++ src/main.py
@@ -18,35 +18,38 @@ seq_len = 4
 batch_size = 64
 num_workers = 8
 
+training_days = 18
+test_days = 20 - training_days
+
 TIMESTAMP = "Timestamp"
 TRAIN_DATA_PATH = "dataset/NIST_cleaned_train.csv"
 TEST_DATA_PATH = "dataset/NIST_cleaned_test.csv"
 DATA_PATH = "dataset/NIST_cleaned.csv"
 
-training_days = 18
-test_days = 20 - training_days
 
-
-try:
-    train_data = pd.read_csv(TRAIN_DATA_PATH)
-    test_data = pd.read_csv(TEST_DATA_PATH)
-except FileNotFoundError as e:
+def main():
     try:
-        df = pd.read_csv(DATA_PATH)
-        train_data, test_data = split_data_train_and_test(df, 18, 20, TIMESTAMP)
+        train_data = pd.read_csv(TRAIN_DATA_PATH)
+        test_data = pd.read_csv(TEST_DATA_PATH)
     except FileNotFoundError as e:
-        print(DATA_PATH + " not found")
+        try:
+            df = pd.read_csv(DATA_PATH)
+            train_data, test_data = split_data_train_and_test(df, training_days, test_days, TIMESTAMP)
+        except FileNotFoundError as e:
+            print(DATA_PATH + " not found")
 
-model = SimpleLSTM(hidden_size, batch_size)
+    model = SimpleLSTM(hidden_size, batch_size)
 
-training_xs, training_ys = create_sequences(train_data, seq_len, TIMESTAMP)
-training_loader = data.DataLoader(data.TensorDataset(training_xs, training_ys), batch_size=batch_size, drop_last=True, num_workers=8)
+    training_xs, training_ys = create_sequences(train_data, seq_len, TIMESTAMP)
+    training_loader = data.DataLoader(data.TensorDataset(training_xs, training_ys), batch_size=batch_size, drop_last=True, num_workers=8)
 
-test_xs, test_ys = create_sequences(train_data, seq_len, TIMESTAMP)
-test_loader = data.DataLoader(data.TensorDataset(test_xs, test_ys), batch_size=batch_size, drop_last=True, num_workers=8)
-timestamps = test_data.values[:, 0]
+    test_xs, test_ys = create_sequences(train_data, seq_len, TIMESTAMP)
+    test_loader = data.DataLoader(data.TensorDataset(test_xs, test_ys), batch_size=batch_size, drop_last=True, num_workers=8)
+    timestamps = test_data.values[:, 0]
 
-trainer = LSTMTrainer(model, learning_rate, timestamps)
-lightning_trainer = L.Trainer(max_epochs=epochs)
-lightning_trainer.fit(trainer, training_loader)
-lightning_trainer.test(trainer, test_loader)
+    trainer = LSTMTrainer(model, learning_rate, timestamps)
+    lightning_trainer = L.Trainer(max_epochs=epochs)
+    lightning_trainer.fit(trainer, training_loader)
+    lightning_trainer.test(trainer, test_loader)
+
+main()
