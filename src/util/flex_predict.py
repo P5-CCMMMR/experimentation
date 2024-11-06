84a519bfe5a8994b886ecd552b8af2ad33d8247e
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2024-11-06 17:32:39 +0100

    probabilistic flex predict

--- src/util/flex_predict.py
+++ src/util/flex_predict.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from scipy.stats import norm
 
 def flex_predict(forecasts, lower_bound, upper_bound, error):
     flex_iter = 0
@@ -12,14 +13,19 @@ def flex_predict(forecasts, lower_bound, upper_bound, error):
     
     return flex_iter
 
-def prob_flex_predict(forecasts, lower_bound, upper_bound, error):
+def prob_flex_predict(forecasts, lower_bound, upper_bound, error, confidence=0.95):
     flex_iter = 0
-    
     for i in range(0, len(forecasts[0])):
-        forecast = np.random.normal(forecasts[0][i], forecasts[1][i]) # 0 being mean and 1 std div
-        if lower_bound + error <= forecast and upper_bound - error >= forecast:    
+        mean = forecasts[0][i]
+        stddev = forecasts[1][i]
+        
+        prob_within_lower = norm.cdf(lower_bound + error, mean, stddev)
+        prob_within_upper = norm.cdf(upper_bound - error, mean, stddev)
+        prob_within = min(prob_within_upper, prob_within_lower)
+        
+        if prob_within >= confidence:
             flex_iter = flex_iter + 1
         else:
-            break
-    
+            break   
+     
     return flex_iter
