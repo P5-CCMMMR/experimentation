151343ce62310c94f761b04e677b9012cfe6b6d7
MadsVind <madsvind7@gmail.com>
2024-11-21 15:09:06 +0100

    monte-carlo works again

--- src/pipelines/models/gru.py
+++ src/pipelines/models/gru.py
@@ -6,12 +6,13 @@ class GRU(Model):
     def __init__(self, hidden_size: int, num_layers: int, input_len: int, horizon_len: int, dropout: float):
         super(GRU, self).__init__(hidden_size, num_layers, input_len, horizon_len, dropout)
         self.gru = nn.GRU(self.input_len, self.hidden_size, self.num_layers, batch_first=True, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, x):
         batch_size = x.size(0)
         h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(x.device)
         out, _ = self.gru(x, h0)
-        out = out[:, -1, :]
+        out = self.dropout(out[:, -1, :])
         return self.fc(out)
     
--- src/pipelines/models/lstm.py
+++ src/pipelines/models/lstm.py
@@ -6,6 +6,7 @@ class LSTM(Model):
     def __init__(self, hidden_size: int, num_layers: int, input_len: int, horizon_len: int, dropout: float):
         super(LSTM, self).__init__(hidden_size, num_layers, input_len, horizon_len, dropout)
         self.lstm = nn.LSTM(self.input_len, self.hidden_size, self.num_layers, batch_first=True, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, x):
@@ -13,6 +14,6 @@ class LSTM(Model):
         h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(x.device)
         out, _ = self.lstm(x, (h0, c0))
-        out = out[:, -1, :]
+        out = self.dropout(out[:, -1, :])
         return self.fc(out)
     
