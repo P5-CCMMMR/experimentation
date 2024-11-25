fix normalization error funcs

src/pipelines/metrics/crps.py
@@ -54,6 +54,8 @@ class NMCRPS(CRPS):
         """
         Normalized Mean Continuous Ranked Probability Score
         """
-        if y.max() == y.min():
-            return torch.tensor(0.0)
-        return MCRPS.calc(mean, stddev, y) / (y.max() - y.min())
+        # Add small term to avoid divison by zero
+        eps = 1e-16
+        denominator = y.max() - y.min() if y.max() != y.min() else torch.tensor(eps)
+        
+        return MCRPS.calc(mean, stddev, y) / denominator

src/pipelines/metrics/lscv.py
@@ -52,6 +52,8 @@ class NMLSCV(MLSCV):
         """
         Normalized Mean Logarithmic Score for Continuous Variables
         """
-        if y.max() == y.min():
-            return torch.tensor(0.0)
-        return MLSCV.calc(mean, stddev, y) / (y.max() - y.min())
+        # Add small term to avoid divison by zero
+        eps = 1e-16
+        denominator = y.max() - y.min() if y.max() != y.min() else torch.tensor(eps)
+        
+        return MLSCV.calc(mean, stddev, y) / denominator

src/pipelines/metrics/rmse.py
@@ -27,6 +27,9 @@ class NRMSE(RMSE):
         """
         Normalized Root Mean Squared Error
         """
-        if y.max() == y.min():
-            return torch.tensor(0.0)
-        return RMSE.calc(y_hat, y) / (y.max() - y.min())
+        
+        # Add small term to avoid divison by zero
+        eps = 1e-16
+        denominator = y.max() - y.min() if y.max() != y.min() else torch.tensor(eps)
+   
+        return RMSE.calc(y_hat, y) / denominator