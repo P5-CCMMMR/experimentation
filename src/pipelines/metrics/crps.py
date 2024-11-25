fix normalization

src/pipelines/metrics/crps.py
@@ -58,4 +58,4 @@ class NMCRPS(TCRPS):
         eps = 1e-16
         denominator = y.max() - y.min() if y.max() != y.min() else torch.tensor(eps)
         
-        return MCRPS.calc(mean, stddev, y) / denominator
+        return MCRPS.calc(mean, stddev, y) - y.min() / denominator

src/pipelines/metrics/lscv.py
@@ -55,4 +55,4 @@ class NMLSCV(MLSCV):
         eps = 1e-16
         denominator = y.max() - y.min() if y.max() != y.min() else torch.tensor(eps)
         
-        return MLSCV.calc(mean, stddev, y) / denominator
+        return MLSCV.calc(mean, stddev, y) - y.min() / denominator

src/pipelines/metrics/rmse.py
@@ -32,4 +32,4 @@ class NRMSE(RMSE):
         eps = 1e-16
         denominator = y.max() - y.min() if y.max() != y.min() else torch.tensor(eps)
    
-        return RMSE.calc(y_hat, y) / denominator
+        return RMSE.calc(y_hat, y) - y.min() / denominator