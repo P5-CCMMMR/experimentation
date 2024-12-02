74047eb13b11760407d029860c5897106280bff4
mtygesen <mikkel.tygesen@gmail.com>
2024-12-02 11:30:20 +0100

    removed print

--- tests/test_cale.py
+++ tests/test_cale.py
@@ -8,7 +8,6 @@ class TestCALE(unittest.TestCase):
         stddev = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5])
         y = torch.tensor([1.05, 2.1, 3.5, 4.5, 6.0])
         result = CALE.calc(mean, stddev, y)
-        print(result)
         self.assertAlmostEqual(result[0], 0.4)
         self.assertAlmostEqual(result[1], 0.8) 
         self.assertAlmostEqual(result[2], 1)
