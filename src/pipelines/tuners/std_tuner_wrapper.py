6cfde22978a025b05836812368dce49bdc4397be
mtygesen <mikkel.tygesen@gmail.com>
2024-12-04 19:48:40 +0100

    rm ws

--- src/pipelines/tuners/std_tuner_wrapper.py
+++ src/pipelines/tuners/std_tuner_wrapper.py
@@ -2,7 +2,6 @@ from .tuner_wrapper import TunerWrapper
 from lightning.pytorch.tuner import Tuner
 
 class StdTunerWrapper(TunerWrapper):
-
     def tune(self, scale_batch_size=False):
         tuner = Tuner(self.trainer)
         if scale_batch_size:
